79a08f88dfb839440a62d68188f7a6e7001b416e
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-17 13:25:33 -0400

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
-  # write code here
+  
+  attr_reader :sides
+  attr_accessor :kind
+
+  def initialize(s1 , s2 , s3)
+    @sides = [s1 , s2 , s3]
+  end
+
+  def kind
+
+    bad = self.sides.min <= 0 || sides[0] + sides[1] <= sides[2] ||  sides[1] + sides[2] <= sides[0] ||  sides[0] + sides[2] <= sides[1]  ? true : false
+
+    if self.sides.uniq.count == 3 && bad == false
+      self.kind = :scalene
+    elsif self.sides.uniq.count == 2 && bad == false
+      self.kind = :isosceles
+    elsif self.sides.uniq.count == 1 && bad == false
+      self.kind = :equilateral
+    elsif  bad == true 
+      begin
+        raise TriangleError
+      end 
+    end
+
+ 
+  end
+  class TriangleError < StandardError
+  end 
 end
